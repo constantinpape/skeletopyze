ea687facc57cd5cc22aaa2b44469ce27fb644131
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-04-20 16:38:02 +0200

    Hacky installation fix

--- compile_wrapper.sh
+++ compile_wrapper.sh
@@ -10,14 +10,14 @@ shift
 echo "Creating skeletopyze build directory in $BUILD_DIR"
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-rm CMakeCache.txt
+#rm CMakeCache.txt
 echo "Calling CMake for sources in $SOURCE_DIR"
 echo "Setting boost search dir to $PREFIX"
-cmake $SOURCE_DIR \
-  -DBOOST_ROOT=${PREFIX} \
-  -DBoost_NO_SYSTEM_PATHS=ON
-  #-DBoost_PYTHON_LIBRARY=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
-  #-DBoost_PYTHON_LIBRARY_RELEASE=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
-  #-DBoost_PYTHON_LIBRARY_DEBUG=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
+#cmake $SOURCE_DIR \
+#  -DBOOST_ROOT=${PREFIX} \
+#  -DBoost_NO_SYSTEM_PATHS=ON
+#  #-DBoost_PYTHON_LIBRARY=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
+#  #-DBoost_PYTHON_LIBRARY_RELEASE=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
+#  #-DBoost_PYTHON_LIBRARY_DEBUG=${PREFIX}/lib/libboost_python.${DYLIB_EXT} \
 echo "Creating target $TARGET_NAME"
 make -j3 "$@" $TARGET_NAME
